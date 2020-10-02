032697f3556c4284031035f5daafa12b49d04735
jmprice12 <69824253+jmprice12@users.noreply.github.com>
2020-10-02 12:44:58 -0500

    gg

--- p5/Unit2/touches/sketch.js
+++ p5/Unit2/touches/sketch.js
@@ -1,4 +1,4 @@
-let numberOfTouches = 0 ;
+let numberOfTouches = 0;
 var img1, img2, img3, img4, img5;
 
 function setup() {
@@ -13,38 +13,38 @@ function setup() {
 
 function draw() {
   clear();
-  numberOfTouches = touches.length;// this automatically comes in
+  numberOfTouches = touches.length; // this automatically comes in
   text(numberOfTouches + ' touches', 5, 10);
 
-  switch(numberOfTouches) {
+  switch (numberOfTouches) {
     case 0:
-      text("no one is petting me give me some lovins", 5, 22) ;
-      image(img1, width / 2, height / 2, 400 ,400 );
-      break ;
+      text("no one is petting me give me some lovins", 5, 22);
+      image(img1, width / 2, height / 2, 400, 400);
+      break;
 
-      case 1:
-       text( "that feel good give me some more scratchins", 5, 22) ;
-       image(img2, width / 2, height / 2, 400 ,400 );
+    case 1:
+      text("that feel good give me some more scratchins", 5, 22);
+      image(img2, width / 2, height / 2, 400, 400);
       // put a picture here
-      break ;
+      break;
 
-      case 2:
-      text("don't be stingy scratch my ear ", 5, 22) ;
-      image(img3, width / 2, height / 2, 400 ,400 );
-            // put a picture here
-      break ;
+    case 2:
+      text("don't be stingy scratch my ear ", 5, 22);
+      image(img3, width / 2, height / 2, 400, 400);
+      // put a picture here
+      break;
 
-      case 3:
-     text("OHHH YEAH!! I love you forever keep it up! ", 5, 22) ;
-     image(img4, width / 2, height / 2, 400 ,400 );
-            // put a picture here
-      break ;
+    case 3:
+      text("OHHH YEAH!! I love you forever keep it up! ", 5, 22);
+      image(img4, width / 2, height / 2, 400, 400);
+      // put a picture here
+      break;
 
-      case 4:
-     text("wait don't stop! ", 5, 22) ;
-     image(img5, width / 2, height / 2, 400 ,400 );
-            // put a picture here
-      break ;
+    case 4:
+      text("wait don't stop! ", 5, 22);
+      image(img5, width / 2, height / 2, 400, 400);
+      // put a picture here
+      break;
 
 
   }
