623a9425ffa023e85e2a44e63688f72af889e562
jmprice12 <69824253+jmprice12@users.noreply.github.com>
2020-11-23 14:56:06 -0600

    fffff

--- p5/Unit4/gyrocollector/sketch.js
+++ p5/Unit4/gyrocollector/sketch.js
@@ -87,27 +87,27 @@ function draw() {
   // MORE DECORATIONS - write that pretty ATK type on top.
   fill('white');
   textFont(f);
-  textSize(80);
+  textSize(20);
   textAlign(CENTER);
   text("Catch A Falling Star", width / 2, 600, windowWidth - 200, windowHeight - 200);
 
 
   // Debugging information -- take this out when you're ready for production!
   // Just a bunch of text commands to display data coming in from addEventListeners
-  textAlign(LEFT);
-  textSize(20);
-  fill('black');
-  text("orientation data:", 25, 25);
-  textSize(15);
-  text("alpha: " + alpha, 25, 50);
-  text("beta: " + beta, 25, 70);
-  text("gamma: " + gamma, 25, 90);
-  textSize(20);
-  text("acceleration data:", 25, 125);
-  textSize(15);
-  text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
-  text("y = " + y, 25, 170);
-  text("z = " + z, 25, 190);
+  //textAlign(LEFT);
+  //textSize(20);
+  //fill('black');
+  //text("orientation data:", 25, 25);
+  //textSize(15);
+  //text("alpha: " + alpha, 25, 50);
+  //text("beta: " + beta, 25, 70);
+  //text("gamma: " + gamma, 25, 90);
+  //textSize(20);
+//  text("acceleration data:", 25, 125);
+//  textSize(15);
+  //text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
+  //text("y = " + y, 25, 170);
+  //text("z = " + z, 25, 190);
 
 
 }
